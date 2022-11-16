5afa16d76a444a4fb198d9292c017302868f9caf
Blz13 <bruno.lazo@outlook.com>
2022-11-16 18:44:16 -0300

    The game is now playable laks the ability to regai n an old username

--- Scripts/game.js
+++ Scripts/game.js
@@ -1,4 +1,6 @@
-const weaponsButtons = document.querySelectorAll('.weapons')
+let gamesPlayed = 0;
+
+const weaponsButtons = document.querySelectorAll('.weapons');
 
 const weapons = [
     
@@ -7,25 +9,83 @@ const weapons = [
     paper = new Weapon ('paper','rock','./Images/Paper.png'),
 
     scissors = new Weapon ('scissors','paper','./Images/Scissors.png')
+
 ]
+
+const resultsArea = document.querySelectorAll(".results");
+
+const scoreArea = document.querySelectorAll(".score");
+
+for (let i = 0; i < scoreArea.length; i++) {
+    scoreArea[i].innerText = users[i].score;
+}
 
 weaponsButtons.forEach( weaponButton => {
     weaponButton.onclick = () => {
         const attackName = weaponButton.dataset.weapon;
-        const attack = weapons.find( weaponSelection => attackName === weaponSelection.weapon);
-        const cpuAttack = attackCPU(attack);
-        result(attack,cpuAttack)
+        const userAttack = weapons.find( weaponSelection => attackName === weaponSelection.weapon);
+        const cpuAttack = attackCPU();
+        result(userAttack,cpuAttack);
+        printResult(userAttack,cpuAttack,resultsArea);
+        showResults();
+        printScores();
     }
 })
 
-function attackCPU (attack) {
+function attackCPU () {
     let randomAttack = Math.floor (Math.random() * weapons.length);
-    if (weapons[randomAttack] === attack) {
-        randomAttack = Math.floor(Math.random () * weapons.length);
-    }
     return (weapons[randomAttack]);
 }
 
-function result(attack,cpuAttack) {
-    
+function result(userAttack,cpuAttack) {
+    if (userAttack.weapon === cpuAttack.weapon) { //cheks for a draw
+        users[0].draw();
+        users[1].draw();
+    } else {
+        if (userAttack.beats === cpuAttack.weapon) { // cheks if user1 won
+            users[0].win();
+        } else { 
+            users[1].win();
+        }
+    }
+}
+
+function printResult(userAttack,cpuAttack,resultsArea) {
+    resultsArea.forEach( eachArea => {
+        const divBox = document.createElement("div");
+        const divImg = document.createElement("img");
+        divBox.setAttribute("class", "resultsBox hidden");
+        divBox.appendChild(divImg);
+        eachArea.insertAdjacentElement("afterbegin", divBox)
+    })
+    resultsArea[0].firstChild.firstChild.setAttribute("src", userAttack.img);
+    resultsArea[2].firstChild.firstChild.setAttribute("src", cpuAttack.img);
+    if (userAttack.weapon === cpuAttack.weapon) { //cheks for a draw
+        resultsArea[1].firstChild.innerText = "draw"
+        resultsArea[0].firstChild.firstChild.classList.add("draw");
+        resultsArea[2].firstChild.firstChild.classList.add("draw");
+    } else {
+        if (userAttack.beats === cpuAttack.weapon) { // cheks if user1 won
+            resultsArea[1].firstChild.innerText = ` ${user1.name} wins `
+            resultsArea[0].firstChild.firstChild.classList.toggle("winner");
+            resultsArea[2].firstChild.firstChild.classList.toggle("looser");
+        } else {
+            resultsArea[1].firstChild.innerText = ` ${user2.name} wins `
+            resultsArea[0].firstChild.firstChild.classList.toggle("looser");
+            resultsArea[2].firstChild.firstChild.classList.toggle("winner");
+        }
+    }
+    resultsArea[1].firstChild.classList.toggle("resultsText")
+}
+
+function showResults() {
+    document.querySelectorAll(".hidden").forEach( result => {
+        result.style.animationDirection = "normal";
+    })
+}
+
+function printScores() {
+    for (let i = 0; i < scoreArea.length; i++) {
+        scoreArea[i].innerText = users[i].score;
+    }    
 }
--- Scripts/script.js
+++ Scripts/script.js
@@ -1,48 +1,27 @@
-let gamesPlayed = 0;
+const icon = document.getElementById("refresh");
 
 const sections = [document.getElementById("menu"), document.getElementById("logIn"), document.getElementById("game")];
 
 const arrowButtons = document.querySelectorAll(".arrow");
 
-const user1NameInput = document.getElementById("userName1");
-
-const user2NameInput = document.getElementById("userName2");
-
 const player1 = document.getElementById("player1");
 
 const player2 = document.getElementById("player2");
 
-const player1Score = document.getElementById("player1Score");
+let user1 = new User ("dksjuhfgkhdsgf");
 
-const player2Score = document.getElementById("player2Score");
+let user2 = new User ("PC");
 
-let gamemode;
+const users = [user1,user2];
 
-const users = [];
 
-let selectedName1 = "dksjuhfgkhdsgf";
+player2.childNodes[0].textContent = (users[1].name) + ":";
 
-let selectedName2 = "PC";
-
-let user1 = new User (selectedName1);
-
-let user2 = new User (selectedName2);
+document.getElementById("userName2").value = users[1].name
 
 arrowButtons.forEach( arrowButton => {  
     arrowButton.onclick = () => {
         let indexActive;
-        switch (arrowButton.id) {
-            case "singlePlayerArrow" :
-                gamemode = 1;
-                player2.childNodes[0].textContent = (user2.name) + ":";
-                player2Score.innerText = user2.score;
-                user2NameInput.value = user2.name;
-                break;
-            case "multiPlayerArrow" :
-                gamemode = 2;
-                user2NameInput.removeAttribute("disabled");
-                break;
-        }
         for (let i = 0; i<sections.length; i++){
             if (sections[i].className == "active"){
                  indexActive = i;
@@ -55,41 +34,23 @@ arrowButtons.forEach( arrowButton => {
                 sections[1].style.animation = "fadeIn 2s ease-in 0s 1 normal forwards";
                 sections[0].className = "inactive";
                 sections[1].className = "active";
-                break;
+            break;
             case 1 :
-                if (gamemode === 2){
-                    if ((!(user1.name === "dksjuhfgkhdsgf")) && (!(user2.name === "PC"))){ 
-                        sections[1].style.animation = "fadeOut 2s ease-in 0s 1 normal forwards";
-                        sections[2].style.animation = "fadeIn 2s ease-in 0s 1 normal forwards";
-                        sections[1].className = "inactive";
-                        sections[2].className = "active";
-                    }
-                } else {
-                    if (!(user1.name === "dksjuhfgkhdsgf")){ 
-                        sections[1].style.animation = "fadeOut 2s ease-in 0s 1 normal forwards";
-                        sections[2].style.animation = "fadeIn 2s ease-in 0s 1 normal forwards";
-                        sections[1].className = "inactive";
-                        sections[2].className = "active";
-                    }
+                if (!(users[0].name === "dksjuhfgkhdsgf")){ 
+                    sections[1].style.animation = "fadeOut 2s ease-in 0s 1 normal forwards";
+                    sections[2].style.animation = "fadeIn 2s ease-in 0s 1 normal forwards";
+                    sections[1].className = "inactive";
+                    sections[2].className = "active";
                 }
-                break;
+            break;
         }
     }
 });
 
-user1NameInput.onchange = () => {
-    selectedName1 = user1NameInput.value;
-    user1.name = selectedName1;
-    player1.childNodes[0].textContent = user1.name + ":";
-    player1Score.innerText = user1.score;
-    localStorage.setItem("user1", JSON.stringify(user1));
-    //la idea es recuperar los datos del local storage en caso de reiniciar
+document.getElementById("userName1").onchange = () => {
+    users[0].name = document.getElementById("userName1").value;
+    player1.childNodes[0].textContent = users[0].name + ":";
+    localStorage.setItem("user1", JSON.stringify(users[0].name));
 };
 
-user2NameInput.onchange = () => {
-    selectedName2 = user2NameInput.value;
-    user2.name = selectedName2;
-    player2.childNodes[0].textContent = (user2.name) + ":";
-    player2Score.innerText = user2.score;
-    localStorage.setItem("user2", JSON.stringify(user2));
-};
+icon.addEventListener("click" , () => { icon.classList.toggle("spin") });
